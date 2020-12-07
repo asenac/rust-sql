8efe8772f48514627cc38af6bbc3f462222ebfbf
Andres Senac <andres@senac.es>
2020-12-07 11:56:24 +0100

    split modules

---
+++ src/rewrite_engine.rs
@@ -0,0 +1,43 @@
+pub trait Rule<T> {
+    fn name(&self) -> &'static str;
+    fn apply_top_down(&self) -> bool;
+    fn condition(&mut self, obj: &T) -> bool;
+    fn action(&mut self, obj: &mut T) -> Option<T>;
+    // @todo prune
+}
+
+pub trait Traverse<T> {
+    fn descend_and_apply(rule: &mut dyn Rule<T>, target: &mut T);
+}
+
+fn apply_rule<T>(rule: &mut dyn Rule<T>, target: &mut T) -> Option<T> {
+    if rule.condition(target) {
+        rule.action(target)
+    } else {
+        None
+    }
+}
+
+pub fn deep_apply_rule<T: Clone + Traverse<T>>(
+    rule: &mut dyn Rule<T>,
+    target: &mut T,
+) -> Option<T> {
+    if !rule.apply_top_down() {
+        T::descend_and_apply(rule, target);
+    }
+    let result = apply_rule(rule, target);
+    match result {
+        Some(mut c) => {
+            if rule.apply_top_down() {
+                T::descend_and_apply(rule, &mut c);
+            }
+            Some(c)
+        }
+        None => {
+            if rule.apply_top_down() {
+                T::descend_and_apply(rule, target);
+            }
+            None
+        }
+    }
+}
